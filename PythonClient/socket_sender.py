64b73512176e0bca1a6e734cd88775ad22ccf781
Aleksandr <alxsave@gmail.com>
2018-05-17 13:44:18 +0000

    One line sending with cts key

--- PythonClient/socket_sender.py
+++ PythonClient/socket_sender.py
@@ -1,15 +1,20 @@
 import socket
+
 import base64
+
 port = 8888
+
 client = socket.socket()
+
 client.connect(('10.1.17.65', port))
-prefix = b'\x00\x00\x00\x03'
+
 while True:
-    
+
     tosend = input()
-    mymessage = b'rcv\x00\x00\x00\x20' + base64.b64encode(bytes(tosend, 'UTF-8'))
-#    print(mymessage)
-    a = client.send(prefix)
-    a = client.send(mymessage)
-       
-client.close() 
+
+    a = client.send(((bytes("\x00\x00\x00\x03cts\x00\x00\x00\x20", "utf-8") + base64.b64encode(bytes(tosend, "UTF-8")))))
+
+
+
+client.close()
+
