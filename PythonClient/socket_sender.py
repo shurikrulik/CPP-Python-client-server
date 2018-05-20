230df5e9c7fa6b456aaaa6c2056d4ed5cb857b34
Aleksandr <shurikrulik@list.ru>
2018-05-20 22:41:42 +0300

    Add files via upload
    Variable Key added

--- PythonClient/socket_sender.py
+++ PythonClient/socket_sender.py
@@ -1,20 +1,18 @@
 import socket
-
 import base64
-
 port = 8888
-
 client = socket.socket()
+client.connect(('localhost', port))
 
-client.connect(('10.1.17.65', port))
+key="cts"
 
 while True:
 
-    tosend = input()
-
-    a = client.send(((bytes("\x00\x00\x00\x03cts\x00\x00\x00\x20", "utf-8") + base64.b64encode(bytes(tosend, "UTF-8")))))
-
-
-
+	data = input()
+	key_length = len(key)
+	data_encoded = base64.b64encode(bytes(data, "UTF-8"))
+	data_length = len(data_encoded)
+	message = key_length.to_bytes(4, byteorder='big') + bytes(key, "UTF-8") + data_length.to_bytes(4, byteorder='big') + data_encoded
+	client.send(message)
+        
 client.close()
-
