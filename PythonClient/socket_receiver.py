65fd3b8eb1c18ca71b4ffbaafc3f7ad46457dc34
Aleksandr <shurikrulik@list.ru>
2018-05-21 09:36:22 +0300

    Update socket_receiver.py
    add empty string case check

--- PythonClient/socket_receiver.py
+++ PythonClient/socket_receiver.py
@@ -16,26 +16,25 @@ expected_key = b'cts'
 while True:
 
         data = client.recv(4096)
-        while len(data) > 0:
-            if stage == 1: 
-                key_size = int.from_bytes(data[:4], 'big')
-                data = data[4:]
-                stage = 2
-            if stage == 2 and data != b'':
-                key = data[:key_size]
-                data = data[key_size:]
-                stage = 3
-            if stage == 3 and data != b'':
-                msg_size = int.from_bytes(data[:4], 'big')
-                data = data[4:]
-                stage = 4
-            if stage == 4 and data != b'':
-                msg = data
-                stage = 1
-                data = b''
-                if key == expected_key:
-                    print(base64.b64decode(msg))
-
-
+        if data != b'':
+            while len(data) > 0:
+                if stage == 1: 
+                    key_size = int.from_bytes(data[:4], 'big')
+                    data = data[4:]
+                    stage = 2
+                if stage == 2 and data != b'':
+                    key = data[:key_size]
+                    data = data[key_size:]
+                    stage = 3
+                if stage == 3 and data != b'':
+                    msg_size = int.from_bytes(data[:4], 'big')
+                    data = data[4:]
+                    stage = 4
+                if stage == 4 and data != b'':
+                    msg = data
+                    stage = 1
+                    data = b''
+                    if key == expected_key:
+                        print(base64.b64decode(msg))
 
 client.close()
