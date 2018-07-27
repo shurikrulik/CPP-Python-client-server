7fc8e5133e2e9a9bdda3c50cbbc7b156ab38d39a
Aleksandr <shurikrulik@list.ru>
2018-07-27 16:50:46 +0300

    Update radial.py

--- radial.py
+++ radial.py
@@ -1,26 +1,31 @@
-import csv
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
+import csv
 
-fig = plt.figure(figsize=(4,4))
+
+fig = plt.figure(figsize=(9,9))
 ax = fig.add_subplot(111, projection='polar')
-ax.set_ylim(0,100000)
+ax.set_ylim(0,70000)
 
-
-theta = np.linspace(0,190, num=571)
+data = np.random.rand(571)*10000
+theta = np.linspace(0,3.31613, num=571)
 l,  = ax.plot([],[])
 
 def update(i):
     global data
-    for row in rows[i-1:i]:
-        l.set_data(theta, row )
+    row = rows[i:i+1]
+
+    # for i in range(571):
+    #     if row[i] =="":
+    #         row[i]=0
+    # data += np.add( data, row, out=data, casting="unsafe")
+    l.set_data(theta, row)
+    print(i)
     return l,
 
 with open('/home/aleksandr/Documents/lastmeasurement.csv', newline='') as File:
     reader = csv.reader(File)
     rows = list(reader)
-    for row in rows[1:2]:
-        data = row
-ani = animation.FuncAnimation(fig, update, frames=3796, interval=100, blit=True)
+ani = animation.FuncAnimation(fig, update, frames=3305, interval=100, blit=True)
 plt.show()
